53791f4f1c1c11ff4a8db3f790f890c7fbb7ccb9
nanov <nanovche1@gmail.com>
2021-02-24 16:04:02 +0200

    refactored launcher to start with gorm.db

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"electronic-school-diary/env"
+	"electronic-school-diary/model"
+	"log"
+
+	"gorm.io/gorm"
 )
 
 func main() {
 
-	env := new(env.Env)
-	env.InitDB()
+	var db *gorm.DB
+	var err error
+	if db, err = model.NewDB(); err != nil {
+		log.Fatal(err)
+	}
+
+	env := env.Env{
+		Db: db,
+	}
+
+	model.Migrate(&env)
 
 }
