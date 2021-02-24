1b6867fda4010025d3c4f65899bbc3a315c7383d
nanov <nanovche1@gmail.com>
2021-02-24 16:03:29 +0200

    replaced sql.DB with gorm.Db in struct definition

--- env/env.go
+++ env/env.go
@@ -1,19 +1,20 @@
 package env
 
 import (
-	"database/sql"
 	"html/template"
 	"log"
+
+	"gorm.io/gorm"
 )
 
 type Env struct {
-	db        *sql.DB
-	logger    *log.Logger
-	templates *template.Template
+	Db        *gorm.DB
+	Logger    *log.Logger
+	Templates *template.Template
 }
 
-func (env *Env) InitDB() error {
-	var err error
-	env.db, err = sql.Open("mysql", "root:23sl_@/school?parseTime=true")
-	return err
-}
+// func (env *Env) InitDB() error {
+// 	var err error
+// 	env.db, err = sql.Open("mysql", "root:23sl_@/school?parseTime=true")
+// 	return err
+// }
