e3258ae93d624335cfa13c2a8520601ce0a2334c
nanov <nanovche1@gmail.com>
2021-02-24 16:36:19 +0200

    renamed file models to teacher and placed it in separate dir teacher

---
+++ model/database.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"electronic-school-diary/env"
+	"fmt"
+
+	"github.com/go-gorm/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDB() (*gorm.DB, error) {
+
+	db, err := gorm.Open(mysql.Open("root:23sl_@/electronic_diary?parseTime=true"), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize database session: %s", err)
+	}
+	return db, nil
+
+}
+
+func Migrate(env *env.Env) {
+	env.Db.AutoMigrate(&Teacher{})
+}
