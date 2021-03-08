923a923c98dbec091fa796ddbc2a408d02511ed3
nanov <nanovche1@gmail.com>
2021-03-08 09:32:24 +0200

    forgot to track changes here...

--- model/entities/entities.go
+++ model/entities/entities.go
@@ -29,8 +29,7 @@ type Parent struct {
 
 type Student struct {
 	Student_ID uint `gorm:"primaryKey"`
-	FirstName  string
-	LastName   string
+	FullName  string
 	Number     uint8
 }
 
@@ -45,10 +44,24 @@ type Mark struct {
 	Teacher_ID uint `gorm:"foreignKey:teacher_ID"`
 	Subject_ID uint `gorm:"foreignKey:subject_id"`
 	Value      int
+	Inserted_At time.Time
 }
 
 type Teacher_Subject struct{
 	Teacher_Subject_ID uint `gorm:"primaryKey"`
 	Subject_ID uint `gorm:"foreignKey:subject_id"`
 	Teacher_ID uint `gorm:"foreignKey:teacher_ID"`
+}
+
+type WeekDay struct {
+	Weekday_classes_ID uint `gorm:"primaryKey"`
+	Consecutive_class int
+	Subject_ID uint `gorm:"foreignKey:subject_id"`
+	Date_recorded time.Time
+}
+
+type Term struct{
+	Term_id uint `gorm:"primaryKey"`
+	Start_date time.Time
+	End_date time.Time
 }
